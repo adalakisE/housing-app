f76d7fdd1b2c7cc65c4f9a658ad03243d8ea0123
Eleftherios Adalakis <adalakis.el@gmail.com>
2023-09-13 18:18:30 +0200

    added loading spinner

--- src/components/View.js
+++ src/components/View.js
@@ -5,19 +5,32 @@ import MapWrapper from "./Features/MapWrapper";
 import ComparisonFooter from "./Listings/ComparisonFooter/ComparisonFooter";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import { useSelector } from "react-redux";
 import "./ViewStyles.scss";
 
 function View() {
+  const stateItems = useSelector((state) => state.storedItems);
+
   return (
-    <div className="view">
+    <div className={`view${stateItems.length ? "" : "--loading"}`}>
       <TopBar />
-      <Box sx={{ display: "flex" }}>
-        <CircularProgress />
-      </Box>
-      <div className="middle-page-wrapper">
+      <div
+        className={`middle-page-wrapper${stateItems.length ? "" : "--loading"}`}
+      >
         <Listings />
         <MapWrapper />
       </div>
+
+      <Box
+        sx={{
+          position: "absolute",
+          right: "calc(50vw - 48px)",
+          top: "calc(50vh - 40px)",
+          display: () => ("stateItems.length" ? "none" : "block"),
+        }}
+      >
+        <CircularProgress size={80} />
+      </Box>
       <ComparisonFooter />
     </div>
   );
