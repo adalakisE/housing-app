0d869f74fb4be1f2121de782634faf1f5223ea87
Eleftherios Adalakis <adalakis.el@gmail.com>
2023-09-13 18:14:58 +0200

    added loading spinner

--- src/components/View.js
+++ src/components/View.js
@@ -3,12 +3,17 @@ import TopBar from "./TopBar/TopBarWrapper";
 import Listings from "./Listings/ListingsWrapper";
 import MapWrapper from "./Features/MapWrapper";
 import ComparisonFooter from "./Listings/ComparisonFooter/ComparisonFooter";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 import "./ViewStyles.scss";
 
 function View() {
   return (
     <div className="view">
       <TopBar />
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
       <div className="middle-page-wrapper">
         <Listings />
         <MapWrapper />
