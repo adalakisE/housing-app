6a3d3a1b767c79dff4823b8ac1962f90e49f80b0
Eleftherios Adalakis <adalakis.el@gmail.com>
2024-05-16 17:31:33 +0200

    added useFetchItems hook

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,42 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, useSearchParams, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { storeItems } from "src/redux/actions/toDoActions";
-import { fetching } from "src/redux/actions/toDoActions";
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import useFetchItems from "src/hooks/useFetchItems";
 import Search from "src/assets/Icons/search.png";
 import "./SearchBarStyles.scss";
 
-// const URL = "http://localhost:5500"; //nodejs server; can be accessed with Live Server vscode extension
-// const URL = "http://localhost:8080"; //springboot server
-const URL = "https://fox-house-backend.onrender.com"; //live serve from Render.com
-
 function SearchBar() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const filters = useSelector((state) => state.appReducer.filters);
-
-  const [searchParams, setSearchParams] = useSearchParams({
-    title: "",
-  });
-
-  const title = searchParams.get("title");
-
-  const [stateTitle, setStateTitle] = useState(title);
-
-  const getListings = async () => {
-    dispatch(fetching(true));
-    navigate(`/mainpage/search${location.search}`);
-
-    const response = await fetch(`${URL}/feed/items/${location.search}`)
-      .then((response) => response.json())
-      .then((data) => dispatch(storeItems(data)))
-      .catch((err) => console.log(err));
-
-    dispatch(fetching(false));
-    return response;
-  };
+  // eslint-disable-next-line no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams({ title: "" });
+  const [stateTitle, setStateTitle] = useState(searchParams.get("title"));
+  const { setParams } = useFetchItems(stateTitle); // Pass stateTitle and an empty object as filters
 
   const handleChange = (e) => {
     setStateTitle(e.target.value);
@@ -48,29 +20,6 @@ function SearchBar() {
     }
   };
 
-  const setParams = () => {
-    setSearchParams((prev) => {
-      prev.set("title", stateTitle);
-
-      for (const [key, value] of Object.entries(filters)) {
-        prev.set(key, value);
-      }
-
-      return prev;
-    });
-  };
-
-  useEffect(() => {
-    if (location.search.length) {
-      /** make it run only ONCE when on the start page */
-      getListings();
-    }
-    // if(autoSearch){
-    //   getListings();
-    // }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location.search]);
-
   return (
     <div className="search-bar__container">
       <input
@@ -80,35 +29,18 @@ function SearchBar() {
         onKeyDown={handleKeyPress}
         onChange={handleChange}
       />
-
-      {location.pathname === "/" ? (
-        <button
-          id="search-btn"
-          onClick={setParams}
-          className="search-bar__search-btn-container"
-        >
-          <p className="search-bar__search-btn-text--mobile">
-            Search Properties
-          </p>
-          <img
-            className="search-bar__search-btn-icon"
-            src={Search}
-            alt="search"
-          />
-        </button>
-      ) : (
-        <button
-          id="search-btn"
-          onClick={setParams}
-          className="search-bar__search-btn-container--main-page"
-        >
-          <img
-            className="search-bar__search-btn-icon"
-            src={Search}
-            alt="search"
-          />
-        </button>
-      )}
+      <button
+        id="search-btn"
+        onClick={setParams}
+        className="search-bar__search-btn-container"
+      >
+        <p className="search-bar__search-btn-text--mobile">Search Properties</p>
+        <img
+          className="search-bar__search-btn-icon"
+          src={Search}
+          alt="search"
+        />
+      </button>
     </div>
   );
 }
---
+++ src/hooks/useFetchItems.js
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSearchParams, useNavigate, useLocation } from "react-router-dom";
+import { storeItems, fetching } from "src/redux/actions/toDoActions";
+
+// const URL = "http://localhost:5500"; //nodejs server; can be accessed with Live Server vscode extension
+// const URL = "http://localhost:8080"; //springboot server
+const URL = "https://fox-house-backend.onrender.com";
+
+function useFetchItems(stateTitle) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  // eslint-disable-next-line no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams({ title: "" });
+
+  const fetchItems = async () => {
+    dispatch(fetching(true));
+    navigate(`/mainpage/search${location.search}`);
+
+    try {
+      const response = await fetch(`${URL}/feed/items/${location.search}`);
+      const data = await response.json();
+      dispatch(storeItems(data));
+    } catch (error) {
+      console.log(error);
+    }
+
+    dispatch(fetching(false));
+  };
+
+  const setParams = () => {
+    setSearchParams((prev) => {
+      prev.set("title", stateTitle);
+      return prev;
+    });
+  };
+
+  useEffect(() => {
+    if (location.search.length) {
+      fetchItems();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.search]);
+
+  return { setParams };
+}
+
+export default useFetchItems;
